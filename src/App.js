e5e0d27493231df5de173ef1eac1495582586abe
Owenzo7 <owenlee545@gmail.com>
2023-04-04 21:18:27 +0300

    Done with implementing the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+function Shop() {
+  return <h1>I am the shop page</h1>;
+}
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
