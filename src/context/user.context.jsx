89fec0082c1db364786231e118eb4838daa2c52a
Owenzo7 <owenlee545@gmail.com>
2023-04-18 20:27:54 +0300

    saved all the changes

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,7 +1,11 @@
 import { createContext, useState } from "react";
 
 // Actual value you want to access
-export const UserContext = createContext({});
+export const UserContext = createContext({
+    currentUser:null,
+    setCurrentUser: () => null,
+
+});
 
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
