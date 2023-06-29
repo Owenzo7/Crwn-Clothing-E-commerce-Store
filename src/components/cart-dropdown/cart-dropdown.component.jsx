e6fca91c8bea6eeacf2d8dd8c655c7c0c95fb1a8
Owenzo7 <owenlee545@gmail.com>
2023-06-29 20:09:24 +0300

    done with add to cart P1

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./cart-dropdown.styles.scss";
 import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 function CartDropdown() {
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items" />
+      <div className="cart-items">
+      
+      
+      </div>
 
       <Button>GO TO CHECKOUT</Button>
     </div>
