989a3486794f88ebc00fe4fed79dfd27e747c45a
Owenzo7 <owenlee545@gmail.com>
2023-07-07 16:00:24 +0300

    solved the error for once and for all

--- src/context/product.context.jsx
+++ src/context/product.context.jsx
@@ -2,7 +2,7 @@
 import {createContext,useState, useEffect}from "react";
 import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils.js";
 
-import SHOP_DATA from "../shopData.js";
+
 
 
 export const ProductsContext = createContext({
@@ -15,11 +15,7 @@ export const ProductsProvider = ({ children }) => {
 
     const [products, setProducts] = useState([])
 
-    useEffect(() => {
-
-
-      addCollectionAndDocuments('categories', SHOP_DATA )
-    }, [])
+    
     const value = {products}
   return <ProductsContext.Provider value={value}> {children} </ProductsContext.Provider>;
 };
