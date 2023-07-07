719d9161644ef214af29c28dcd6b462781e6ad64
Owenzo7 <owenlee545@gmail.com>
2023-07-07 22:07:39 +0300

    done for today

--- src/routes/Shop/shop.component.jsx
+++ src/routes/Shop/shop.component.jsx
@@ -1,16 +1,15 @@
 /* eslint-disable no-unused-vars */
 import { Route, Routes } from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
 
 import "./shop.styles.scss";
 
 function Shop() {
-  
   return (
     <Routes>
-
-    <Route index element={<CategoriesPreview />} />
-    
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category  />} />
     </Routes>
   );
 }
