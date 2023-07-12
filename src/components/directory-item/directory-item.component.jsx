5118d9e979a8a0b55566e0ec1ac720c4f692cf5c
Owenzo7 <owenlee545@gmail.com>
2023-07-12 20:30:07 +0300

    someone please check on my error

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,14 +1,14 @@
 import {
   Body,
-  BackgroundImage,
   DirectoryItemContainer,
+  BackgroundImage,
 } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
   return (
     <DirectoryItemContainer>
-    <BackgroundImage imageUrl={imageUrl} />
+      <BackgroundImage imageUrl={imageUrl} />
 
       <Body>
         <h2>{title}</h2>
